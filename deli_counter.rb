14b8b48d5859667e96289dedd8891d1859a22091
Arielle D <domingu4@gmail.com>
2018-09-06 13:44:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(katz_deli)
+  line_array = []
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    katz_deli.each.with_index(1) do |value, index|
+      line_array.push("#{index}. #{value}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli,new_person)
+end
